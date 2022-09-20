eb8293f49f827731c564ccf30d42f561866a19d7
owuraku-zenas <awukuzenas@gmail.com>
2022-09-20 02:54:33 +0000

    Making the type column of categories unique

--- database/migrations/2022_08_29_195737_create_categories_table.php
+++ database/migrations/2022_08_29_195737_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->string('type')->unique();
             $table->timestamps();
         });
     }
