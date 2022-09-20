f4bd487fe21cab033f2a009409113c92064883db
owuraku-zenas <awukuzenas@gmail.com>
2022-09-20 02:50:21 +0000

    company store and index complete

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Company;
-use Illuminate\Support\Facades\Request;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CompanyController extends Controller
 {
@@ -14,17 +18,25 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        //
-    }
+        try {
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+            $company = Company::all();
+
+            $response = [
+                'success' => true,
+                'data' => $company,
+                'message' => "Companies retrieved successfully"
+            ];
+
+            return response()->json($response, 200);
+        } catch (Exception $error) {
+
+            return response()->json([
+                'success' => false,
+                'data' => $error,
+                'message' => "Falied to retrieve jobs"
+            ], 500);
+        }
     }
 
     /**
@@ -36,6 +48,62 @@ class CompanyController extends Controller
     public function store(Request $request)
     {
         //
+        if (User::findOrFail($request->user()->id)->company) {
+            return response()->json([
+                'success' => false,
+                'message' => "Company Already Exists for User"
+            ], 422);
+        }
+
+
+        try {
+            $validator = Validator::make($request->all(), [
+                'website' => "required",
+                'about' => "required",
+                'linked_in' => "required",
+                'twitter' => "string",
+                'instagram' => "string",
+                'category' => "string"
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'data' => $validator->errors()->all()
+                ], 422);
+            }
+
+            // Check if category in DB
+            if (Category::where('type', $request->category)->exists()) {
+                $category = Category::where('type', $request->category)->first();
+            } else {
+                $category = Category::create([
+                    'type' => $request->category
+                ]);
+            }
+
+            $company = Company::create([
+                'website' => $request->website,
+                'about' =>  $request->about,
+                'linked_in' =>  $request->linked_in,
+                'user_id' => $request->user()->id,
+                'category_id' => $category->id,
+                'instagram' =>  $request->instagram || null,
+                'twitter' => $request->twitter || null,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'data' => $company,
+                'message' => "Company Created Successfully"
+            ]);
+        } catch (Exception $error) {
+            return response()->json([
+                'success' => false,
+                'data' => $error->getMessage(),
+                'message' => "Falied to create job"
+            ], 500);
+        }
     }
 
     /**
